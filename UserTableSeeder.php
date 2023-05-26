89d05b8481b75342605952959c1a48c9b35cafc2
Monir Hossain <51879416+mhsharkar2011@users.noreply.github.com>
2023-05-26 10:55:20 +0600

    Create UserTableSeeder.php
    User Table Seeder added

---
+++ UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Vehicle;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // User::factory(10)->create();
+
+        $user = User::create([
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'email' => 'superadmin@email.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+
+        $user->assignRole('driver','admin','user');
+    }
+}
